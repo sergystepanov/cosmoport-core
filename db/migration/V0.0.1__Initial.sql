351099728b625733cc7e1f72175b067c219058b6
Sergy Stepanov <sergystepanov@gmail.com>
2017-01-23 03:17:07 +0300

    Add initial database migrations and data

---
+++ db/migration/V0.0.1__Initial.sql
@@ -0,0 +1,29 @@
+--
+-- Initial core schema
+--
+-- @encoding UTF-8
+--
+
+--
+-- Stores scheduled events of the timetable
+--
+CREATE TABLE TIMETABLE (
+  id             INTEGER        NOT NULL PRIMARY KEY                                          AUTOINCREMENT,
+  departure_time INTEGER        NOT NULL                                                      DEFAULT (0),
+  type           TEXT           NOT NULL                                                      DEFAULT (''),
+  duration       INTEGER        NOT NULL                                                      DEFAULT (1),
+  destinition    TEXT           NOT NULL                                                      DEFAULT (''),
+  cost           DECIMAL(10, 2) NOT NULL                                                      DEFAULT (1.00),
+  status         TEXT           NOT NULL CHECK (
+    status IN ('pending', 'opened', 'closed', 'canceled', 'boarding', 'departed', 'back', 'preorder',
+               'inactive'))                                                                   DEFAULT ('pending'),
+  gate_no        INTEGER        NOT NULL                                                      DEFAULT (0),
+  passangers_max INTEGER        NOT NULL                                                      DEFAULT (1),
+  bought         INTEGER        NOT NULL CHECK (bought <= passangers_max)                     DEFAULT (0),
+  date_added     TIMESTAMP      NOT NULL                                                      DEFAULT (CURRENT_TIMESTAMP)
+);
+
+--
+-- SQLite performance tuning
+--
+PRAGMA foreign_keys = ON;
